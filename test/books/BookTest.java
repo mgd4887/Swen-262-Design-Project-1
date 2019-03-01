0f1eb042e31a36298bb615b3b2374f9e7a5dc39b
mgd4887 <mgd4887@rit.edu>
2019-03-01 18:05:41 -0500

    fixed gitignore

---
+++ test/books/BookTest.java
@@ -0,0 +1,59 @@
+package books;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+        Author author = mock(Author.class);
+        Book CuT = new Book()
+    }
+
+    @Test
+    public void getISBN() {
+    }
+
+    @Test
+    public void getPublishedDate() {
+    }
+
+    @Test
+    public void getPageCount() {
+    }
+
+    @Test
+    public void getNumCopies() {
+    }
+
+    @Test
+    public void getPurchasedDate() {
+    }
+
+    @Test
+    public void getNumCopiesCheckedOut() {
+    }
+
+    @Test
+    public void getAuthor() {
+    }
+
+    @Test
+    public void getPublisher() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+}
