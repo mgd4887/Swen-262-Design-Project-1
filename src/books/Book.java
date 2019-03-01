612c6c7464849e6bc8e1ab664ad88cee1b049b12
Michael Dolan <mgd4887@cs.rit.edu>
2019-03-01 12:43:30 -0500

    fixed missing ;
    he was found

--- src/books/Book.java
+++ src/books/Book.java
@@ -118,7 +118,7 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return ISBN == book.ISBN
+        return ISBN == book.ISBN;
     }
 
     /**
