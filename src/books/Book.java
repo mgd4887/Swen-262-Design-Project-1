df88fc9e86549b559197b48c7199db488d81e6e9
Michael Dolan <mgd4887@cs.rit.edu>
2019-03-01 12:29:53 -0500

    Books
    added book class and inventory

--- src/books/Book.java
+++ src/books/Book.java
@@ -1,4 +1,64 @@
 package books;
 
+import java.util.Objects;
+
 public class Book {
+    private Author author;
+    private Publisher publisher;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author=" + author +
+                ", publisher=" + publisher +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) &&
+                Objects.equals(publisher, book.publisher) &&
+                Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, publisher, name);
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public Book(String name, Author author, Publisher publisher){
+        this.name = name;
+        this.author = author;
+        this.publisher = publisher;
+    }
 }
--- src/books/Inventory.java
+++ src/books/Inventory.java
@@ -1,4 +1,15 @@
 package books;
 
+import java.util.HashMap;
+
 public class Inventory {
+    private HashMap<Integer, Book> books;
+
+    public Inventory() {
+        this.books = new HashMap<>();
+    }
+
+    void addBook(Book book){
+        books.put(book.hashCode(), book);
+    }
 }
