09db06fedfcd99a26170f16d9a8e0a794b487ab7
zxc8027 <zxc8027@g.rit.edu>
2019-03-01 23:26:34 -0500

    Added book registering and unit tests

--- src/books/Inventory.java
+++ src/books/Inventory.java
@@ -3,44 +3,99 @@ package books;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Class representing the inventory of books.
+ *
+ * @author Michael Dolan
+ * @author Zachary Cook
+ */
 public class Inventory {
-    private HashMap<Integer, Book> books;
+    private HashMap<Integer,Book> books;
 
+    /**
+     * Creates the inventory of book.
+     */
     public Inventory() {
         this.books = new HashMap<>();
     }
 
+    /**
+     * Registers a book in the inventory.
+     *
+     * @param book the book to register.
+     */
+    public void registerBook(Book book) {
+        this.books.put(book.getISBN(),book);
+    }
+
+    /**
+     * Returns the book for the ISBN number.
+     *
+     * @param ISBN the ISBN number of the book.
+     *
+     * @return the book with the corresponding ISBN number.
+     */
     public Book getBook(int ISBN){
         return books.get(ISBN);
     }
 
+    /**
+     * Searches the inventory for books by name.
+     *
+     * @param name the name to search.
+     *
+     * @return the list of books found.
+     */
     public ArrayList<Book> getBooks(String name){
+        // Search the books.
         ArrayList<Book> output = new ArrayList<>();
         for (Book book: books.values()){
-            if (book.getName().equals(name)){
+            if (book.getName().contains(name)){
                 output.add(book);
             }
         }
+
+        // Return the books.
         return output;
     }
 
+    /**
+     * Searches the inventory for books by author.
+     *
+     * @param author the author to search.
+     *
+     * @return the list of books found.
+     */
     public ArrayList<Book> getBooks(Author author){
+        // Search the books.
         ArrayList<Book> output = new ArrayList<>();
         for (Book book: books.values()){
             if (book.getAuthor().equals(author)){
                 output.add(book);
             }
         }
+
+        // Return the books.
         return output;
     }
 
+    /**
+     * Searches the inventory for books by publisher.
+     *
+     * @param publisher the publisher to search.
+     *
+     * @return the list of books found.
+     */
     public ArrayList<Book> getBooks(Publisher publisher){
+        // Search the books.
         ArrayList<Book> output = new ArrayList<>();
         for (Book book: books.values()){
             if (book.getPublisher().equals(publisher)){
                 output.add(book);
             }
         }
+
+        // Return the books.
         return output;
     }
 
---
+++ test/books/InventoryTest.java
@@ -0,0 +1,106 @@
+package books;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import time.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the {@link Inventory} class.
+ *
+ * @author Zachary Cook
+ */
+public class InventoryTest {
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Inventory CuT;
+
+    /**
+     * Sets up the unit tests.
+     */
+    @BeforeEach
+    public void setup() {
+        // Create the authors and publishers.
+        Author author1 = new Author("John","Doe");
+        Author author2 = new Author("Jane","Doe");
+        Publisher publisher1 = new Publisher("Publisher 1");
+        Publisher publisher2 = new Publisher("Publisher 2");
+
+        // Create a blank date.
+        Date blankDate = new Date(0,0,0,0,0,0);
+
+        // Create the books.
+        this.book1 = new Book(author1,publisher1,1,blankDate,425,3,1,blankDate,"Test Book");
+        this.book2 = new Book(author1,publisher2,2,blankDate,271,7,7,blankDate,"Test Book: The Sequel");
+        this.book3 = new Book(author2,publisher2,3,blankDate,515,2,0,blankDate,"Other Book");
+
+        // Create the component under testing and register the books.
+        this.CuT = new Inventory();
+        this.CuT.registerBook(this.book1);
+        this.CuT.registerBook(this.book2);
+        this.CuT.registerBook(this.book3);
+    }
+
+    /**
+     * Tests that the constructor works without failing.
+     */
+    @Test
+    public void test_constructor() {
+        new Inventory();
+    }
+
+    /**
+     * Tests the {@link Inventory#getBook(int)} method.
+     */
+    @Test
+    public void test_getBook() {
+        assertEquals(CuT.getBook(1),this.book1,"Book is incorrect.");
+        assertEquals(CuT.getBook(2),this.book2,"Book is incorrect.");
+        assertEquals(CuT.getBook(3),this.book3,"Book is incorrect.");
+        assertNull(CuT.getBook(4),"Book was returned.");
+    }
+
+    /**
+     * Tests the {@link Inventory#getBooks(String)} method by name.
+     */
+    @Test
+    public void test_getBooksName() {
+        assertTrue(CuT.getBooks("Test Book").contains(this.book1),"Book 1 not returned.");
+        assertTrue(CuT.getBooks("Test Book").contains(this.book2),"Book 2 not returned.");
+        assertFalse(CuT.getBooks("Test Book").contains(this.book3),"Book 3 returned.");
+
+        assertFalse(CuT.getBooks("Test Book: The Sequel").contains(this.book1),"Book 1 returned.");
+        assertTrue(CuT.getBooks("Test Book: The Sequel").contains(this.book2),"Book 2 not returned.");
+        assertFalse(CuT.getBooks("Test Book: The Sequel").contains(this.book3),"Book 3 returned.");
+    }
+
+    /**
+     * Tests the {@link Inventory#getBooks(Author)} method by author.
+     */
+    @Test
+    public void test_getBooksAuthor() {
+        assertTrue(CuT.getBooks(new Author("John","Doe")).contains(this.book1),"Book 1 not returned.");
+        assertTrue(CuT.getBooks(new Author("John","Doe")).contains(this.book2),"Book 2 not returned.");
+        assertFalse(CuT.getBooks(new Author("John","Doe")).contains(this.book3),"Book 3 returned.");
+
+        assertFalse(CuT.getBooks(new Author("Jane","Doe")).contains(this.book1),"Book 1 returned.");
+        assertFalse(CuT.getBooks(new Author("Jane","Doe")).contains(this.book2),"Book 2 returned.");
+        assertTrue(CuT.getBooks(new Author("Jane","Doe")).contains(this.book3),"Book 3 not returned.");
+    }
+
+    /**
+     * Tests the {@link Inventory#getBooks(Publisher)} method by publisher.
+     */
+    @Test
+    public void test_getBooksPublisher() {
+        assertTrue(CuT.getBooks(new Publisher("Publisher 1")).contains(this.book1),"Book 1 not returned.");
+        assertFalse(CuT.getBooks(new Publisher("Publisher 1")).contains(this.book2),"Book 2 returned.");
+        assertFalse(CuT.getBooks(new Publisher("Publisher 1")).contains(this.book3),"Book 3 returned.");
+
+        assertFalse(CuT.getBooks(new Publisher("Publisher 2")).contains(this.book1),"Book 1 returned.");
+        assertTrue(CuT.getBooks(new Publisher("Publisher 2")).contains(this.book2),"Book 2 not returned.");
+        assertTrue(CuT.getBooks(new Publisher("Publisher 2")).contains(this.book3),"Book 3 not returned.");
+    }
+}
