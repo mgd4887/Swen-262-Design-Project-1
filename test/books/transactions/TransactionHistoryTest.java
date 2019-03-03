49938d06327651309b0c8886e46aa32ea207c158
zxc8027 <zxc8027@g.rit.edu>
2019-03-03 15:30:02 -0500

    Improved documentation

--- test/books/transactions/TransactionHistoryTest.java
+++ test/books/transactions/TransactionHistoryTest.java
@@ -11,6 +11,12 @@ import user.Visitor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Class for storing transactions.
+ *
+ * @author Michael Dolan
+ * @author Zachary Cook
+ */
 class TransactionHistoryTest {
 
     private TransactionHistory CuT;
@@ -21,10 +27,15 @@ class TransactionHistoryTest {
     private Visitor visitor2;
     private Visitor visitor3;
 
+    /**
+     * Sets up the unit tests.
+     */
     @BeforeEach
-    void setUp() {
+    void setup() {
+        // Create the component under testing.
         CuT = new TransactionHistory();
 
+        // Create the first book.
         Publisher publisher1 = new Publisher("publisher1");
         Date publishDate1 = new Date(0, 0, 0, 0, 0, 0);
         Date purchaseDate1 = new Date(1, 1, 1, 1, 1, 1);
@@ -37,6 +48,7 @@ class TransactionHistoryTest {
         int numCopiesCheckedOut1 = 11;
         book1 = new Book(author1, publisher1, ISBN1, publishDate1, pageCount1, numCopies1, numCopiesCheckedOut1, purchaseDate1, book1Name);
 
+        // Create the the second book.
         Publisher publisher2 = new Publisher("publisher1");
         Date publishDate2 = new Date(0, 0, 0, 0, 0, 0);
         Date purchaseDate2 = new Date(1, 1, 1, 1, 1, 1);
@@ -49,6 +61,7 @@ class TransactionHistoryTest {
         int numCopiesCheckedOut2 = 11;
         book2 = new Book(author2, publisher2, ISBN2, publishDate2, pageCount2, numCopies2, numCopiesCheckedOut2, purchaseDate2, book2Name);
 
+        // Create the third book.
         Publisher publisher3 = new Publisher("publisher1");
         Date publishDate3 = new Date(0, 0, 0, 0, 0, 0);
         Date purchaseDate3 = new Date(1, 1, 1, 1, 1, 1);
@@ -61,35 +74,49 @@ class TransactionHistoryTest {
         int numCopiesCheckedOut3 = 11;
         book2 = new Book(author3, publisher3, ISBN3, publishDate3, pageCount3, numCopies3, numCopiesCheckedOut3, purchaseDate3, book3Name);
 
+        // Create the visitors.
         Name visitor1Name = new Name("visitor", "one");
-        visitor1 = new Visitor("0000000001",visitor1Name,"V1Address", "111-111-1111");
-
+        visitor1 = new Visitor("0000000001",visitor1Name,"V1Address", "1111111111");
         Name visitor2Name = new Name("visitor", "two");
-        visitor2 = new Visitor("0000000002",visitor2Name,"V2Address", "111-111-1111");
-
+        visitor2 = new Visitor("0000000002",visitor2Name,"V2Address", "1111111111");
         Name visitor3Name = new Name("visitor", "three");
-        visitor3 = new Visitor("0000000003",visitor3Name,"V3Address", "111-111-1111");
+        visitor3 = new Visitor("0000000003",visitor3Name,"V3Address", "1111111111");
     }
 
+    /**
+     * Tests the {@link TransactionHistory#registerTransaction(Transaction)} method.
+     */
     @Test
-    void registerTransaction() {
+    void test_registerTransaction() {
 
 
-        registerTransaction(new Transaction(1, visitor1, ));
+        //registerTransaction(new Transaction(1, visitor1, ));
     }
 
+    /**
+     * Tests the {@link TransactionHistory#registerTransaction(Transaction)} method.
+     */
     @Test
-    void getTransaction() {
+    void test_getTransaction() {
     }
 
+    /**
+     * Tests the {@link TransactionHistory#registerTransaction(Transaction)} method.
+     */
     @Test
     void getTransactionsByVisitor() {
     }
 
+    /**
+     * Tests the {@link TransactionHistory#getTransactionsCheckedOutOn(Date)} method.
+     */
     @Test
     void getTransactionsCheckedOutOn() {
     }
 
+    /**
+     * Tests the {@link TransactionHistory#getTransactionsDueOn(Date)} method.
+     */
     @Test
     void getTransactionsDueOn() {
     }
