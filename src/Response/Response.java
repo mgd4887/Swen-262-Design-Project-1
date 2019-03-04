beb08ca0d3f099d00287616e1b4793b07c98de59
zxc8027 <zxc8027@g.rit.edu>
2019-03-03 21:08:05 -0500

    Implemented Response class

--- src/Response/Response.java
+++
@@ -1,7 +0,0 @@
-package Response;
-
-import java.util.List;
-
-public interface Response{
-    String perform(List<String> params);
-}
---
+++ src/response/Response.java
@@ -0,0 +1,29 @@
+package response;
+
+/**
+ * Class representing a response. The base class (this) only
+ * returns a static string given to the constructor.
+ *
+ * @author Zachary Cook
+ */
+public class Response {
+    private String response;
+
+    /**
+     * Creates the response.
+     *
+     * @param response the response to give, including ending characters.
+     */
+    public Response(String response) {
+        this.response = response;
+    }
+
+    /**
+     * Returns the response.
+     *
+     * @return the response.
+     */
+    public String getResponse() {
+        return this.getResponse();
+    }
+}
