aa071261f170f4a8c9b3eaff92121a8b8e1beea7
Bendrix Bailey <bendrix.bailey@gmail.com>
2019-03-31 10:44:45 -0400

    Completion of the menu page formatting

--- src/Client/MenuPage.java
+++ src/Client/MenuPage.java
@@ -1,5 +1,6 @@
 package Client;
 
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -22,6 +23,11 @@ public class MenuPage extends Page {
         super(clientApplication, LBMS);
     }
 
+    /**
+     * This method creates and sets the layout for all the buttons for the main menu.
+     *
+     * @return returns the borderpane layout so the client can display it.
+     */
     @Override
     public Node getRoot() {
 
@@ -29,19 +35,24 @@ public class MenuPage extends Page {
         BorderPane outerPane = new BorderPane();
 
         //create menu text
-        Text menuText = new Text("Welcome\r\nselect a menu");
+        Text menuText = new Text("Welcome\r\nSelect a menu");
         menuText.setFont(Font.font("Verdana", 20));
-        outerPane.setTop(menuText);
+        HBox textPane = new HBox();
+        textPane.getChildren().add(menuText);
+        outerPane.setTop(textPane);
+        textPane.setAlignment(Pos.CENTER);
 
         //create center grid pane of options
         GridPane centerPane = new GridPane();
         outerPane.setCenter(centerPane);
+        centerPane.setAlignment(Pos.CENTER);
 
         //create button for time page
         HBox timeBox = new HBox();
         Button timeButton = new Button("Time");
         timeButton.setOnMouseClicked(mouseEvent -> clientApplication.changePage(new TimePage(clientApplication, LBMS)));
         timeBox.getChildren().addAll(timeButton);
+        timeBox.setAlignment(Pos.CENTER);
 
 
         //create button for visit page
@@ -49,47 +60,54 @@ public class MenuPage extends Page {
         Button visitButton = new Button("Visit");
         visitButton.setOnMouseClicked(mouseEvent -> clientApplication.changePage(new VisitPage(clientApplication, LBMS)));
         visitBox.getChildren().addAll(visitButton);
+        visitBox.setAlignment(Pos.CENTER);
 
         //create button for store page
         HBox storeBox = new HBox();
         Button storeButton = new Button("Store");
         storeButton.setOnMouseClicked(mouseEvent -> clientApplication.changePage(new StorePage(clientApplication, LBMS)));
         storeBox.getChildren().addAll(storeButton);
+        storeBox.setAlignment(Pos.CENTER);
 
         //create button for borrow page
         HBox borrowBox = new HBox();
         Button borrowButton = new Button("Borrow");
         borrowButton.setOnMouseClicked(mouseEvent -> clientApplication.changePage(new BorrowPage(clientApplication, LBMS)));
         borrowBox.getChildren().addAll(borrowButton);
+        borrowBox.setAlignment(Pos.CENTER);
 
         //create button for Status page
         HBox statusBox = new HBox();
         Button statusButton = new Button("Status");
         statusButton.setOnMouseClicked(mouseEvent -> clientApplication.changePage(new StatusPage(clientApplication, LBMS)));
         statusBox.getChildren().addAll(statusButton);
+        statusBox.setAlignment(Pos.CENTER);
 
         //create logout button
         HBox logoutBox = new HBox();
         Button logoutButton = new Button("Logout");
         logoutButton.setOnMouseClicked(mouseEvent -> clientApplication.logout());
         logoutBox.getChildren().addAll(logoutButton);
+        logoutBox.setAlignment(Pos.CENTER);
 
         //create button for Register page
         HBox registerBox = new HBox();
         Button registerButton = new Button("Register new user");
         registerButton.setOnMouseClicked(mouseEvent -> clientApplication.changePage(new RegisterVisitorPage(clientApplication, LBMS)));
         registerBox.getChildren().addAll(registerButton);
+        registerBox.setAlignment(Pos.CENTER);
 
         //add buttons to gridpane
-        centerPane.add(timeBox,0,0);
-        centerPane.add(visitBox,0,1);
-        centerPane.add(storeBox,1,0);
-        centerPane.add(borrowBox,1,1);
-        centerPane.add(statusBox,2,0);
-        centerPane.add(logoutBox,2,1);
-        centerPane.add(registerBox,3,0);
+        centerPane.add(timeBox,0,1);
+        centerPane.add(visitBox,0,2);
+        centerPane.add(storeBox,0,3);
+        centerPane.add(borrowBox,0,4);
+        centerPane.add(statusBox,0,5);
+        centerPane.add(logoutBox,0,6);
+        centerPane.add(registerBox,0,0);
         centerPane.setVgap(10);
         centerPane.setHgap(10);
+        centerPane.setAlignment(Pos.CENTER);
 
         return (outerPane);
     }
